63e072da385176bf7b58c4b6387268fb4225feed
Kyung Lee <kyung.lee.official@outlook.com>
2024-07-12 15:08:13 +0800

    feat: support markdown for Orama's AI answer

--- src/app/search/orama/Ask.tsx
+++ src/app/search/orama/Ask.tsx
@@ -2,7 +2,7 @@ import { useMemo, useState } from "react";
 import { oramaClient } from "./orama-client";
 import dayjs from "dayjs";
 import { nanoid } from "nanoid";
-import { Bubble } from "./Bubble";
+import { Bubble } from "./bubble/Bubble";
 
 export const Ask = () => {
 	const [conversation, setConversation] = useState<any[]>([]);
@@ -13,16 +13,89 @@ export const Ask = () => {
 			inferenceType: "documentation",
 			initialMessages: [],
 			events: {
-				onMessageChange: (messages) => console.log({ messages }),
-				onMessageLoading: (loading) => console.log({ loading }),
-				onAnswerAborted: (aborted) => console.log({ aborted }),
-				onSourceChange: (sources) => console.log({ sources }),
+				// onMessageChange: (messages) => console.log({ messages }),
+				// onMessageLoading: (loading) => console.log({ loading }),
+				// onAnswerAborted: (aborted) => console.log({ aborted }),
+				// onSourceChange: (sources) => console.log({ sources }),
 			},
 		});
 	}, []);
 
+	const askQuestion = async () => {
+		setConversation((conversation) => [
+			...conversation,
+			{
+				type: "q",
+				bubbleId: nanoid(),
+				state: "fulfilled",
+				content: question,
+				timestamp: dayjs().format("YYYY-MM-DD HH:mm:ss"),
+			},
+		]);
+		const bubbleId = nanoid();
+		setConversation((conversation) => [
+			...conversation,
+			{
+				type: "a",
+				bubbleId,
+				state: "pending",
+				content: undefined,
+				timestamp: undefined,
+			},
+		]);
+		try {
+			const answer = await cachedAnswerSession.ask({
+				term: question,
+			});
+			setConversation((conversation) => {
+				return conversation.map((bubble) => {
+					if (bubble.bubbleId === bubbleId) {
+						return {
+							...bubble,
+							state: "fulfilled",
+							content: answer,
+							timestamp: dayjs().format("YYYY-MM-DD HH:mm:ss"),
+						};
+					}
+					return bubble;
+				});
+			});
+		} catch (error) {
+			setConversation((conversation) => {
+				return conversation.map((bubble) => {
+					if (bubble.bubbleId === bubbleId) {
+						return {
+							...bubble,
+							state: "error",
+							content: "An error occurred",
+							timestamp: dayjs().format("YYYY-MM-DD HH:mm:ss"),
+						};
+					}
+					return bubble;
+				});
+			});
+		}
+	};
+
 	return (
 		<div className="flex-[0_1_50%] flex flex-col gap-4">
+			<div
+				className="flex flex-col h-[80dvh] p-4 gap-6
+				bg-neutral-200/60
+				overflow-y-auto"
+			>
+				{conversation.map((bubble, i) => {
+					return (
+						<Bubble
+							key={i}
+							type={bubble.type}
+							state={bubble.state}
+							timestamp={bubble.timestamp}
+							content={bubble.content}
+						/>
+					);
+				})}
+			</div>
 			<div className="flex gap-4">
 				<textarea
 					className="w-full p-2
@@ -33,87 +106,13 @@ export const Ask = () => {
 					}}
 				/>
 				<button
+					type="submit"
 					className="p-2
 					bg-neutral-200"
-					onClick={async () => {
-						setConversation((conversation) => [
-							...conversation,
-							{
-								type: "q",
-								bubbleId: nanoid(),
-								state: "fulfilled",
-								content: question,
-								timestamp: dayjs().format(
-									"YYYY-MM-DD HH:mm:ss"
-								),
-							},
-						]);
-						const bubbleId = nanoid();
-						setConversation((conversation) => [
-							...conversation,
-							{
-								type: "a",
-								bubbleId,
-								state: "pending",
-								content: undefined,
-								timeStamp: undefined,
-							},
-						]);
-						try {
-							const answer = await cachedAnswerSession.ask({
-								term: question,
-							});
-							setConversation((conversation) => {
-								return conversation.map((bubble) => {
-									if (bubble.bubbleId === bubbleId) {
-										return {
-											...bubble,
-											state: "fulfilled",
-											content: answer,
-											timeStamp: dayjs().format(
-												"YYYY-MM-DD HH:mm:ss"
-											),
-										};
-									}
-									return bubble;
-								});
-							});
-						} catch (error) {
-							setConversation((conversation) => {
-								return conversation.map((bubble) => {
-									if (bubble.bubbleId === bubbleId) {
-										return {
-											...bubble,
-											state: "error",
-											content: "An error occurred",
-											timeStamp: dayjs().format(
-												"YYYY-MM-DD HH:mm:ss"
-											),
-										};
-									}
-									return bubble;
-								});
-							});
-						}
-					}}
+					onClick={askQuestion}
 				>
 					Ask
 				</button>
-			</div>
-			<div
-				className="flex flex-col min-h-8 p-4 gap-4
-				bg-neutral-100"
-			>
-				{conversation.map((bubble, i) => {
-					return (
-						<Bubble
-							key={i}
-							type={bubble.type}
-							state={bubble.state}
-							content={bubble.content}
-						/>
-					);
-				})}
 			</div>
 		</div>
 	);
---
+++ src/app/search/orama/bubble/Bubble.tsx
@@ -0,0 +1,104 @@
+import { useEffect, useState } from "react";
+import { unified } from "unified";
+import remarkParse from "remark-parse";
+import remarkRehype from "remark-rehype";
+import rehypeStringify from "rehype-stringify";
+
+type BubbleType = {
+	type: "q" | "a";
+	state: "fulfilled" | "pending" | "error";
+	content?: string;
+	timestamp?: string;
+};
+
+const Row = (props: {
+	type: "q" | "a";
+	timestamp: string | undefined;
+	children: React.ReactNode;
+}) => {
+	return (
+		<div className={`flex flex-col gap-2`}>
+			<p
+				className="flex justify-center
+				text-sm text-neutral-400"
+			>
+				{props.timestamp}
+			</p>
+			<div
+				className={`flex ${
+					props.type === "q" ? "justify-end" : "justify-start"
+				}`}
+			>
+				<div
+					className={`w-4/5 p-2
+					${props.type === "q" ? "bg-blue-200" : "bg-green-200"}
+					`}
+				>
+					{props.children}
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export const Bubble = (props: BubbleType) => {
+	const { type, state, content, timestamp } = props;
+	const [mdxContent, setMdxContent] = useState({ __html: "" });
+
+	async function compile() {
+		if (content) {
+			const processor = unified();
+			processor.use(remarkParse).use(remarkRehype).use(rehypeStringify);
+			const trimmed = content.replace("[object Object]", "");
+			const result = await processor.process(trimmed);
+			const resultWithLinks = result
+				.toString()
+				.replace("<a", "<a class='text-blue-500'");
+			setMdxContent({ __html: resultWithLinks });
+		}
+	}
+
+	useEffect(() => {
+		if (type === "a") {
+			compile();
+		}
+	}, [content]);
+
+	switch (type) {
+		case "q":
+			return (
+				<Row type="q" timestamp={timestamp}>
+					{content}
+				</Row>
+			);
+			break;
+		case "a":
+			if (state === "pending") {
+				return (
+					<Row type="a" timestamp={timestamp}>
+						...
+					</Row>
+				);
+			}
+			if (state === "error") {
+				return (
+					<Row type="a" timestamp={timestamp}>
+						❗{content}❗
+					</Row>
+				);
+			}
+			if (state === "fulfilled") {
+				return (
+					<Row type="a" timestamp={timestamp}>
+						<div
+							dangerouslySetInnerHTML={mdxContent}
+							className="flex flex-col gap-2"
+						/>
+					</Row>
+				);
+			}
+			break;
+		default:
+			break;
+	}
+};
