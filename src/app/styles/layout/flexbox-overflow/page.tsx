cca50021be227fc256e068f6fc362329f95a1d11
Kyung Lee <kyung.lee.official@outlook.com>
2024-08-13 18:12:49 +0800

    chore: update flexbox-overflow

--- src/app/styles/layout/flexbox-overflow/page.tsx
+++ src/app/styles/layout/flexbox-overflow/page.tsx
@@ -22,6 +22,26 @@ const Page = () => {
 					Loremipsumdolorsitametconsectetur
 				</div>
 			</div>
+			<h2>Children of flexbox are set to `min-w-0` with overflow-x-auto</h2>
+			<div
+				className="flex justify-between w-full h-16 gap-2
+				text-neutral-300
+				bg-neutral-800
+				overflow-x-auto"
+			>
+				<div
+					className="min-w-0
+					bg-green-600/40"
+				>
+					Loremipsumdolorsitametconsectetur
+				</div>
+				<div
+					className="min-w-0
+					bg-green-600/40"
+				>
+					Loremipsumdolorsitametconsectetur
+				</div>
+			</div>
 			<h2>Children of flexbox use default width (`min-w-[auto]`)</h2>
 			<div
 				className="flex justify-between w-full h-16 gap-2
