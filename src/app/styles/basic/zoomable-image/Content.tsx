54fc021e27797d321445ca6b132b1f7d561fab12
Kyung Lee <kyung.lee.official@outlook.com>
2024-12-29 18:02:44 +0800

    style: ...

--- src/app/styles/basic/zoomable-image/Content.tsx
+++ src/app/styles/basic/zoomable-image/Content.tsx
@@ -7,10 +7,12 @@ const Content = () => {
 		<div className="flex flex-col w-full min-h-svh p-10 gap-20">
 			<h1 className="text-4xl">Zoomable Image</h1>
 			<ZoomableImage
+				mode="regular"
 				src={"/images/styles/image/2000x300.jpg"}
 				width={300}
 			/>
 			<ZoomableImage
+				mode="regular"
 				src={"/images/styles/image/300x2000.jpg"}
 				width={100}
 			/>
--- src/app/styles/basic/zoomable-image/ZoomableImage.tsx
+++ src/app/styles/basic/zoomable-image/ZoomableImage.tsx
@@ -1,51 +1,116 @@
-import { useRef, useState } from "react";
+import { ImgHTMLAttributes, useRef, useState } from "react";
 import { createPortal } from "react-dom";
+import { DeleteIcon, PreviewIcon } from "./Icons";
 
 type ImageProps = {
-	src: string;
-	alt: string;
-	/* width of the image without zoom */
-	width?: number;
+	/**
+	 * regular: zoomable, but without zoom and delete button when hovered
+	 * upload: zoomable, with zoom and delete button when hovered
+	 */
+	mode: "regular" | "upload";
 	className?: string;
 };
 
-export const ZoomableImage = (props: ImageProps) => {
-	const { src, alt, width, className } = props;
+export const ZoomableImage = (
+	props: ImgHTMLAttributes<HTMLImageElement> & ImageProps
+) => {
+	const {
+		mode,
+		src,
+		alt,
+		/* width of the image without zoom */ width,
+		className,
+	} = props;
 
 	const imageRef = useRef<HTMLImageElement>(null);
 
 	const [isZoomOut, setIsZoomOut] = useState(false);
 
-	return (
-		<div>
-			<button
-				className="w-fit"
-				onClick={() => {
-					setIsZoomOut(true);
-				}}
-			>
-				<img ref={imageRef} src={src} width={width} alt={alt} />
-			</button>
-			{isZoomOut &&
-				createPortal(
-					<div
-						className="fixed top-0 right-0 bottom-0 left-0
-						flex justify-center items-center
-						bg-black/50
-						z-10"
+	switch (mode) {
+		case "regular":
+			return (
+				<div>
+					<button
+						className="w-fit"
 						onClick={() => {
-							setIsZoomOut(false);
+							setIsZoomOut(true);
 						}}
 					>
-						<div
-							className="flex justify-center items-center w-fit max-w-[95%] h-[90svh]
-							overflow-auto hide-scrollbar"
-						>
-							<img src={src} alt={alt} className={className} />
+						<img ref={imageRef} src={src} width={width} alt={alt} />
+					</button>
+					{isZoomOut &&
+						createPortal(
+							<div
+								className="fixed top-0 right-0 bottom-0 left-0
+								flex justify-center items-center
+								bg-black/50
+								z-10"
+								onClick={() => {
+									setIsZoomOut(false);
+								}}
+							>
+								<div
+									className="flex justify-center items-center w-fit max-w-[95%] h-[90svh]
+									overflow-auto hide-scrollbar"
+								>
+									<img
+										src={src}
+										alt={alt}
+										className={className}
+									/>
+								</div>
+							</div>,
+							document.body
+						)}
+				</div>
+			);
+		case "upload":
+			return (
+				<div>
+					<div className="relative">
+						<img ref={imageRef} src={src} width={width} alt={alt} />
+						<div className="flex justify-center items-center gap-4">
+							<button
+								onClick={() => {
+									setIsZoomOut(true);
+								}}
+							>
+								<PreviewIcon className="w-4 h-4" />
+							</button>
+							<button
+								onClick={() => {
+									setIsZoomOut(true);
+								}}
+							>
+								<DeleteIcon className="w-4 h-4" />
+							</button>
 						</div>
-					</div>,
-					document.body
-				)}
-		</div>
-	);
+					</div>
+					{isZoomOut &&
+						createPortal(
+							<div
+								className="fixed top-0 right-0 bottom-0 left-0
+								flex justify-center items-center
+								bg-black/50
+								z-10"
+								onClick={() => {
+									setIsZoomOut(false);
+								}}
+							>
+								<div
+									className="flex justify-center items-center w-fit max-w-[95%] h-[90svh]
+									overflow-auto hide-scrollbar"
+								>
+									<img
+										src={src}
+										alt={alt}
+										className={className}
+									/>
+								</div>
+							</div>,
+							document.body
+						)}
+				</div>
+			);
+	}
 };
