f8539e3a546db7295ea30d5741ac50191fd33718
Kyung Lee <kyung.lee.official@outlook.com>
2024-12-29 21:18:56 +0800

    feat: update ZoomableImage to support upload mode

--- src/app/styles/basic/zoomable-image/ZoomableImage.tsx
+++ src/app/styles/basic/zoomable-image/ZoomableImage.tsx
@@ -1,30 +1,39 @@
 import { ImgHTMLAttributes, useRef, useState } from "react";
 import { createPortal } from "react-dom";
 import { DeleteIcon, PreviewIcon } from "./Icons";
+import { DeleteConfirmDialog } from "@/app/components/delete-confirmation/DeleteConfirmDialog";
 
-type ImageProps = {
-	/**
-	 * regular: zoomable, but without zoom and delete button when hovered
-	 * upload: zoomable, with zoom and delete button when hovered
-	 */
-	mode: "regular" | "upload";
-	className?: string;
-};
+/**
+ * regular: zoomable, but without zoom and delete button when hovered
+ * upload: zoomable, with zoom and delete button when hovered
+ */
+type ImageProps =
+	| { mode: "regular"; className?: string }
+	| {
+			mode: "upload";
+			question: string;
+			/* the function to call when the delete operation is confirmed in the dialog */
+			onDelete: Function;
+			className?: string;
+	  };
 
 export const ZoomableImage = (
 	props: ImgHTMLAttributes<HTMLImageElement> & ImageProps
 ) => {
 	const {
-		mode,
 		src,
 		alt,
 		/* width of the image without zoom */ width,
+		mode,
 		className,
 	} = props;
 
 	const imageRef = useRef<HTMLImageElement>(null);
 
 	const [isZoomOut, setIsZoomOut] = useState(false);
+	const [showThumbnailMask, setShowThumbnailMask] = useState(false);
+	/* show delete dialog */
+	const [showDelete, setShowDelete] = useState(false);
 
 	switch (mode) {
 		case "regular":
@@ -65,26 +74,41 @@ export const ZoomableImage = (
 				</div>
 			);
 		case "upload":
+			const { question, onDelete } = props;
 			return (
 				<div>
-					<div className="relative">
+					<div
+						className="relative"
+						onMouseOver={() => {
+							setShowThumbnailMask(true);
+						}}
+						onMouseOut={() => {
+							setShowThumbnailMask(false);
+						}}
+					>
 						<img ref={imageRef} src={src} width={width} alt={alt} />
-						<div className="flex justify-center items-center gap-4">
-							<button
-								onClick={() => {
-									setIsZoomOut(true);
-								}}
+						{showThumbnailMask && (
+							<div
+								className="absolute top-0 right-0 bottom-0 left-0
+								flex justify-center items-center gap-4
+								bg-black/50"
 							>
-								<PreviewIcon className="w-4 h-4" />
-							</button>
-							<button
-								onClick={() => {
-									setIsZoomOut(true);
-								}}
-							>
-								<DeleteIcon className="w-4 h-4" />
-							</button>
-						</div>
+								<button
+									onClick={() => {
+										setIsZoomOut(true);
+									}}
+								>
+									<PreviewIcon size={16} />
+								</button>
+								<button
+									onClick={() => {
+										setShowDelete(true);
+									}}
+								>
+									<DeleteIcon size={16} />
+								</button>
+							</div>
+						)}
 					</div>
 					{isZoomOut &&
 						createPortal(
@@ -110,6 +134,15 @@ export const ZoomableImage = (
 							</div>,
 							document.body
 						)}
+					<DeleteConfirmDialog
+						show={showDelete}
+						setShow={setShowDelete}
+						question={question}
+						onDelete={() => {
+							setShowDelete(false);
+							onDelete();
+						}}
+					/>
 				</div>
 			);
 	}
