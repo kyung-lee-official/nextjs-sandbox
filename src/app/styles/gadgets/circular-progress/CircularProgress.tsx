90d99dba6430c05674f0e6d17d574472916f85d8
Kyung Lee <kyung.lee.official@outlook.com>
2025-01-12 16:57:31 +0800

    fix: prevent tiny tick in CircularProgress when progress is 0

--- src/app/styles/gadgets/circular-progress/CircularProgress.tsx
+++ src/app/styles/gadgets/circular-progress/CircularProgress.tsx
@@ -12,20 +12,23 @@ export const CircularProgress = (props: { size: number; progress: number }) => {
 					strokeWidth={strokeWidth}
 					className="stroke-black/10"
 				/>
-				<circle
-					cx="50%"
-					cy="50%"
-					r="42%"
-					fill="none"
-					strokeWidth={strokeWidth}
-					pathLength={
-						100
-					} /* pathLength sets the length of the path, which is used to calculate the length of the dash array */
-					strokeDasharray={`${progress}, 100`}
-					strokeLinecap="round"
-					className="stroke-black/40
-					origin-center -rotate-90"
-				/>
+				{!!progress && (
+					/* progress bar will leave a tiny tick even if progress is 0, because strokeLinecap is set to round */
+					<circle
+						cx="50%"
+						cy="50%"
+						r="42%"
+						fill="none"
+						strokeWidth={strokeWidth}
+						pathLength={
+							100
+						} /* pathLength sets the length of the path, which is used to calculate the length of the dash array */
+						strokeDasharray={`${progress}, 100`}
+						strokeLinecap="round"
+						className="stroke-black/40
+						origin-center -rotate-90"
+					/>
+				)}
 			</svg>
 		</div>
 	);
