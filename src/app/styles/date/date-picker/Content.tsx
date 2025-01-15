bea43590f7c4d3768691cda07156c0abf8870d44
Kyung Lee <kyung.lee.official@outlook.com>
2025-01-15 23:35:31 +0800

    docs: add explanation for time-processing conventions

--- src/app/styles/date/date-picker/Content.tsx
+++ src/app/styles/date/date-picker/Content.tsx
@@ -13,17 +13,75 @@ export const Content = () => {
 
 	return (
 		<div className="flex flex-col gap-3">
-			<div>
-				<h2>date picker</h2>
-				<DatePicker date={date} setDate={setDate} />
+			<h1 className="text-lg">Time consistency</h1>
+			<div
+				className="w-1/2 p-3
+				border-dashed border-black border-2"
+			>
+				<p>
+					When dealing with time, you should always assume that server
+					only accepts UTC time, because the server side does not know
+					the timezone of the client.
+				</p>
+				<div>
+					<h2
+						className="p-1 mb-4
+						border-dashed border-black border-2"
+					>
+						date picker {date.toISOString()}
+					</h2>
+					<DatePicker date={date} setDate={setDate} />
+				</div>
 			</div>
-			<div>
-				<h2>month picker</h2>
-				<MonthPicker date={month} setDate={setMonth} />
-			</div>
-			<div>
-				<h2>year picker</h2>
-				<YearPicker date={year} setDate={setYear} />
+			<div
+				className="w-1/2 p-3
+				border-dashed border-black border-2"
+			>
+				<p>
+					Time inconsistency could happen when referring to a time
+					range instead of a specific time. For example, year and
+					month.
+				</p>
+				<p>
+					Let us say your timezone is UTC+8, and an entity is created
+					at 2025-01-01 01:00:00 (local), the corresponding UTC time
+					in database will be 2024-12-31T17:00:00.000Z, if your try to
+					search entities created in year 2025, you are actually
+					searching entities created at 2024-12-31T16:00:00.000Z to
+					2025-12-31T15:59:59.999Z, which gives you correct results
+					based on your timezone.
+				</p>
+				<p>
+					That is to say, when searching entities within a time range,
+					you should always specify the start and end time before
+					sending the request. Because the frontend knows the timezone
+					of the user, while the backend does not.
+				</p>
+				<p>
+					You cannot say{" "}
+					<i>hey server, give me entities created in year 2025</i>,
+					because the <b>server does not know your timezone</b>.
+				</p>
+				<div className="mb-4">
+					<h2
+						className="p-1 mb-4
+						border-dashed border-black border-2"
+					>
+						month picker {month.toISOString()} -{" "}
+						{month.add(1, "month").toISOString()}
+					</h2>
+					<MonthPicker date={month} setDate={setMonth} />
+				</div>
+				<div>
+					<h2
+						className="p-1 mb-4
+						border-dashed border-black border-2"
+					>
+						year picker {year.toISOString()} -{" "}
+						{year.add(1, "year").toISOString()}
+					</h2>
+					<YearPicker date={year} setDate={setYear} />
+				</div>
 			</div>
 		</div>
 	);
--- src/app/styles/date/date-range-picker/Content.tsx
+++ src/app/styles/date/date-range-picker/Content.tsx
@@ -1,7 +1,10 @@
 "use client";
 
 import dayjs from "dayjs";
-import { DateRange, DateRangePicker } from "./date-range-picker/DateRangePicker";
+import {
+	DateRange,
+	DateRangePicker,
+} from "./date-range-picker/DateRangePicker";
 import { useState } from "react";
 
 export const Content = () => {
@@ -12,8 +15,45 @@ export const Content = () => {
 
 	return (
 		<div>
-			<h2>date range picker</h2>
-			<DateRangePicker range={range} setRange={setRange} />
+			<h1 className="text-lg">Time consistency</h1>
+			<div
+				className="w-1/2 p-3
+				border-dashed border-black border-2"
+			>
+				<p>
+					Time inconsistency could happen when referring to a time
+					range instead of a specific time. For example, year and
+					month.
+				</p>
+				<p>
+					Let us say your timezone is UTC+8, and an entity is created
+					at 2025-01-01 01:00:00 (local), the corresponding UTC time
+					in database will be 2024-12-31T17:00:00.000Z, if your try to
+					search entities created in year 2025, you are actually
+					searching entities created at 2024-12-31T16:00:00.000Z to
+					2025-12-31T15:59:59.999Z, which gives you correct results
+					based on your timezone.
+				</p>
+				<p>
+					That is to say, when searching entities within a time range,
+					you should always specify the start and end time before
+					sending the request. Because the frontend knows the timezone
+					of the user, while the backend does not.
+				</p>
+				<p>
+					You cannot say{" "}
+					<i>hey server, give me entities created in year 2025</i>,
+					because the <b>server does not know your timezone</b>.
+				</p>
+				<h2
+					className="p-1 mb-4
+					border-dashed border-black border-2"
+				>
+					date range picker {range.start.toISOString()} -{" "}
+					{range.end.toISOString()}
+				</h2>
+				<DateRangePicker range={range} setRange={setRange} />
+			</div>
 		</div>
 	);
 };
