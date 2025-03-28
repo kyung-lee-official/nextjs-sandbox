fc40dbf790e46b9c7950f9228b2786246c2b768f
Kyung Lee <kyung.lee.official@outlook.com>
2025-03-29 00:17:07 +0800

    feat: add positive-only Integer Input functionality

--- src/app/styles/form-and-input/integer-input/Content.tsx
+++ src/app/styles/form-and-input/integer-input/Content.tsx
@@ -5,6 +5,7 @@ import { IntegerInput } from "./integer-input/IntegerInput";
 
 export const Content = () => {
 	const [unlimited, setUnlimited] = useState(0);
+	const [positiveOnly, setPositiveOnly] = useState(0);
 	return (
 		<div className="m-8">
 			<h1 className="text-2xl font-bold mb-4">Integer Input Component</h1>
@@ -26,6 +27,19 @@ export const Content = () => {
 					placeholder="unlimited"
 				/>
 				<p className="mt-2 text-gray-500">Current value: {unlimited}</p>
+			</div>
+			<div>
+				<IntegerInput
+					value={positiveOnly}
+					onChange={(e) => {
+						setPositiveOnly(e as number);
+					}}
+					placeholder="positive only"
+					min={0}
+				/>
+				<p className="mt-2 text-gray-500">
+					Current value: {positiveOnly}
+				</p>
 			</div>
 		</div>
 	);
