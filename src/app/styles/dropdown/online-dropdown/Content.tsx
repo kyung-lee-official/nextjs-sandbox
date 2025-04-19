0e06309dc3c49da4d5a6ce2425b563a936c3e3a2
Kyung Lee <kyung.lee.official@outlook.com>
2025-04-19 23:42:44 +0800

    feat: update Content component to support multi-selection and enhance option deselection handling

--- src/app/styles/dropdown/online-dropdown/Content.tsx
+++ src/app/styles/dropdown/online-dropdown/Content.tsx
@@ -10,13 +10,29 @@ type OptionType = {
 };
 
 export const Content = () => {
-	const [selected, setSelected] = useState<OptionType | null>(null);
+	const [selected, setSelected] = useState<OptionType | OptionType[] | null>(
+		null
+	);
+
+	const [selectedMulti, setSelectedMulti] = useState<
+		OptionType | OptionType[] | null
+	>([]);
+
+	/* function to handle deselection of an option */
+	const handleDeselect = (option: OptionType) => {
+		if (Array.isArray(selectedMulti)) {
+			setSelectedMulti(
+				selectedMulti.filter((item) => item.id !== option.id)
+			);
+		}
+	};
+
 	return (
 		<div
-			className="flex gap-10 h-svh
+			className="flex gap-x-5
 			bg-neutral-700"
 		>
-			<div className="w-[500px] p-4 space-y-4">
+			<div className="min-w-[400px] p-4">
 				<Dropdown<OptionType>
 					placeholder="Search for an option..."
 					selected={selected}
@@ -24,6 +40,34 @@ export const Content = () => {
 					endpoint={`${process.env.NEXT_PUBLIC_NESTJS}/mock-data/online-dropdown`}
 					labelKey="name"
 				/>
+			</div>
+			<div className="min-w-[400px] p-4">
+				<Dropdown<OptionType>
+					placeholder="Search for options..."
+					selected={selectedMulti}
+					setSelected={setSelectedMulti}
+					endpoint={`${process.env.NEXT_PUBLIC_NESTJS}/mock-data/online-dropdown`}
+					labelKey="name"
+					multiple={true}
+				/>
+			</div>
+			<div className="flex flex-wrap gap-1.5 w-full p-4">
+				{selectedMulti &&
+					(selectedMulti as OptionType[]).map((selected, i) => {
+						return (
+							<button
+								key={selected.id}
+								onClick={() => handleDeselect(selected)}
+								className="h-7 px-2 py-1
+								text-white text-sm truncate
+								hover:line-through
+								bg-neutral-600 hover:bg-neutral-500
+								rounded-md cursor-pointer"
+							>
+								{selected.name}
+							</button>
+						);
+					})}
 			</div>
 		</div>
 	);
