748d0ad3b18da5118ab402b327d7632b5ec24041
Kyung Lee <kyung.lee.official@outlook.com>
2025-04-26 19:36:05 +0800

    chore: update data variables in PgContent

--- src/app/chart/visx/bars/PgContent.tsx
+++ src/app/chart/visx/bars/PgContent.tsx
@@ -3,17 +3,16 @@
 import { BarChart } from "./barchart/BarChart";
 
 /* data order matters, it determines the order of the bars */
-const dataP = [
-	{ date: "Apr 01, 2025", salesVolume: 190 },
-	{ date: "Apr 03, 2025", salesVolume: 50 },
-	{ date: "Apr 04, 2025", salesVolume: 200 },
-];
-
-const dataPnN = [
+const dateSalesVolume = [
 	{ date: "Apr 01, 2025", salesVolume: 190 },
 	{ date: "Apr 02, 2025", salesVolume: -50 },
 	{ date: "Apr 03, 2025", salesVolume: 5 },
 	{ date: "Apr 04, 2025", salesVolume: 200 },
+];
+const inStock = [
+	{ date: "Shanghai", salesVolume: 190 },
+	{ date: "Shenzhen", salesVolume: 50 },
+	{ date: "Guangzhou", salesVolume: 200 },
 ];
 
 const svgWidth = 900;
@@ -24,13 +23,13 @@ export const PgContent = () => {
 	return (
 		<div className="p-10 space-y-10">
 			<BarChart
-				data={dataP}
+				data={dateSalesVolume}
 				svgWidth={svgWidth}
 				svgHeight={svgHeight}
 				margin={margin}
 			/>
 			<BarChart
-				data={dataPnN}
+				data={inStock}
 				svgWidth={svgWidth}
 				svgHeight={svgHeight}
 				margin={margin}
