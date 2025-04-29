fe05c87ccdcca62281b04deb053903456f6a9435
Kyung Lee <kyung.lee.official@outlook.com>
2025-04-29 12:53:39 +0800

    feat: add percentage text formatting to PieChart for better data representation

--- src/app/chart/visx/pies/PgContent.tsx
+++ src/app/chart/visx/pies/PgContent.tsx
@@ -18,6 +18,8 @@ const svgHeight = 500;
 const margin = { top: 80, right: 80, bottom: 80, left: 80 };
 
 export const PgContent = () => {
+	const totalInStock = inStock.reduce((sum, item) => sum + item.inStock, 0);
+
 	return (
 		<div className="p-10 space-y-10">
 			<PieChart
@@ -36,6 +38,13 @@ export const PgContent = () => {
 					return convertNumberToHumanReadable(Number(value), {
 						useComma: true,
 					});
+				}}
+				textFormatter={(value, data) => {
+					const percentage = (
+						(Number(data.inStock) / totalInStock) *
+						100
+					).toFixed(2);
+					return `${data.city}, ${percentage}%`;
 				}}
 			/>
 		</div>
