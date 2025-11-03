fef7530146ce56faee5e1290d162383072564809
Kyung Lee <kyung.lee.official@outlook.com>
2025-11-03 08:18:03 +0800

    style: ...

--- src/app/payment/paypal/orders-api-integration/page.tsx
+++ src/app/payment/paypal/orders-api-integration/page.tsx
@@ -1,5 +1,26 @@
+import Link from "next/link";
+
 const Page = () => {
-	return <div>Page</div>;
+	return (
+		<div className="flex flex-col p-4 gap-2">
+			<h1>
+				<Link
+					href="https://docs.paypal.ai/developer/how-to/api/get-started"
+					className="underline decoration-dotted cursor-pointer"
+				>
+					Docs: API
+				</Link>
+			</h1>
+			<h1>
+				<Link
+					href="https://docs.paypal.ai/payments/methods/paypal/api/one-time/orders-api-integration"
+					className="underline decoration-dotted cursor-pointer"
+				>
+					Official docs
+				</Link>
+			</h1>
+		</div>
+	);
 };
 
 export default Page;
--- src/app/payment/paypal/page.tsx
+++ src/app/payment/paypal/page.tsx
@@ -2,27 +2,19 @@ import Link from "next/link";
 
 const Page = () => {
 	return (
-		<div className="p-4">
+		<div className="flex flex-col p-4 gap-2">
+			<Link
+				href="/payment/paypal/orders-api-integration"
+				className="underline decoration-dotted cursor-pointer"
+			>
+				Integrate PayPal directly with the Orders v2 API
+			</Link>
 			<Link
 				href="/payment/paypal/react-paypal-js"
 				className="underline decoration-dotted cursor-pointer"
 			>
 				React PayPal JS
 			</Link>
-			<h2 className="flex gap-3">
-				<Link
-					href="/payment/paypal/orders-api-integration"
-					className="underline decoration-dotted cursor-pointer"
-				>
-					Integrate PayPal directly with the Orders v2 API
-				</Link>
-				<Link
-					href="https://docs.paypal.ai/payments/methods/paypal/api/one-time/orders-api-integration"
-					className="underline decoration-dotted cursor-pointer"
-				>
-					Official docs
-				</Link>
-			</h2>
 		</div>
 	);
 };
